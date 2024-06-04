f65d3ee2877b8d28152acd4e3c8ea66bb6d7be10
yamclaro <yamclaro2019@gmail.com>
2024-06-04 03:06:22 -0300

    Refactor# Ajuste artigos nulos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 module ApplicationHelper
     include Pagy::Frontend
+
+    def render_if(condition, template, record)
+        render template, record if condition
+      end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
-    validates :title, presence: true. leght: {minimum: 5}
-    validates :body, presence: true. leght: {minimum: 10}
+    validates :title, presence: true, length: {minimum: 5}
+    validates :body, presence: true, length: {minimum: 10}
+
+    scope :desc_order, -> { order(created_at: :desc) }
+    scope :without_highlights, ->(ids) { where("id NOT IN(#{ids})") if ids.present? }
 end
